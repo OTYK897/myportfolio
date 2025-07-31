c8b79679b522e8f458f6f9165a8c85a165f58ce9
YK <suntkumi5@gmail.com>
2025-07-31 12:23:38 +0900

    enw

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   transpilePackages: ['motion'],
-  output: 'export', // 静的サイトとして出力
-  basePath: '/myportfolio', // リポジトリ名を指定
-  assetPrefix: '/myportfolio',
+  // output: 'export', // 静的サイトとして出力
+  // basePath: '/myportfolio', // リポジトリ名を指定
+  // assetPrefix: '/myportfolio',
 };
 module.exports = nextConfig;
 
